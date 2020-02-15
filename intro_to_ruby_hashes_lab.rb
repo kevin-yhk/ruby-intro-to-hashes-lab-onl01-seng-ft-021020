9f6ab83f50a038a4cdfa029e5f1bc939a4626ba2
Young Hyun (Kevin) Kim <kevinkim.yhk@gmail.com>
2020-02-15 01:38:42 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -32,7 +32,7 @@ def monopoly_with_third_tier
 	  railroads: {
 	    pieces: 4,
 	    names: {
-	      reading},
+	      reading_},
 	    rent_in_dollars: {
 	      one_piece_owned: 25,
 	      two_pieces_owned: 50,
